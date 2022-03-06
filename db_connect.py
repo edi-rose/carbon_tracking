9ecef89b2ebd6f388044711ccabe1e0eca1d34dd
Edi Rose <edirose1998@gmail.com>
2022-03-06 13:39:31 +1300

    changed to pymysql, added get latest calls

--- db_connect.py
+++ db_connect.py
@@ -1,30 +1,58 @@
+import pymysql as db
 
-import mysql.connector as db
-import datetime
-import main
-
-
-connection = db.connect(host="localhost", port="3306", user="root", password="Th3B3llJar", database="carbon_market_schema")
-if connection.is_connected(): 
-    print("DB is connected")
-else: print("getDb Failed")
-
-date= datetime.datetime
-
-def insertCarbonPrice(price):
-    db= connection.cursor()
-    db.callproc('insert_carbon',[price])
+def insertCarbonPrice(price, diff):
+    connection = connectDB()
+    cursor= connection.cursor(buffered=True)
+    cursor.callproc('insert_carbon',[price, diff])
     connection.commit()
-    return db.rowcount
+    disconnectDB(connection)
+    return
 
-def insertSaltPrice(price):
-    db= connection.cursor()
-    db.callproc('insert_salt',[price])
+def insertSaltPrice(price, diff):
+    connection = connectDB()
+    cursor = connection.cursor(buffered=True)
+    cursor.callproc('insert_salt',[price, diff])
     connection.commit()
-    return db.rowcount
+    disconnectDB(connection)
+    return
 
-prices= main.GetAndRefinePrices()
-carbon_price = prices[0]
-salt_price = prices[1]
+def getLatestCarbonFromDB():
+    connection = connectDB()
+    cursor= connection.cursor()
+    cursor.callproc('get_latest_carbon')
+    result = cursor.fetchall()
+    for r in result:
+        if r is not None:
+            disconnectDB(connection)
+            return r
+    else: 
+        disconnectDB(connection)
+        #hack for none handle :/
+        return [0, 0, 0, 0]
 
 
+def getLatestSaltFromDB():
+    connection = connectDB()
+    cursor= connection.cursor()
+    cursor.callproc('get_latest_salt')
+    result = cursor.fetchall()
+    for r in result:
+        if r is not None:
+            disconnectDB(connection)
+        return r
+    else: 
+        disconnectDB(connection)
+        #hack for none handle :/
+        return [0, 0, 0, 0]
+    
+
+def connectDB():
+    connection = db.connect(host="localhost", port=3306, user="root", password="Th3B3llJar", database="carbon_market_schema")
+    if connection is not None:
+        print('db connected')
+        return connection
+
+def disconnectDB(connection):
+    if connection is not None:
+        connection.close()
+    return
