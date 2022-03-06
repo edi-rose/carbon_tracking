e2523fc6dfa9fe36ee22e2fa7f4b68babd339a03
Edi Rose <edirose1998@gmail.com>
2022-03-06 13:43:14 +1300

    started listening calls and insert logic

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import priceGetters as getter
 import numpy as np 
 import re
+import db_connect as db
 
+def findPrices(): 
+    carbon_previous = db.getLatestCarbonFromDB()[2]
+    salt_previous = db.getLatestSaltFromDB()[2]
 
-def GetAndRefinePrices():
+    current_prices = GetAndRefineNewPrices()
+    carbon_current = current_prices[0]
+    salt_current = current_prices[1]
 
+    if salt_current != salt_previous: 
+        diff = salt_current - salt_previous
+        db.insertSaltPrice(salt_current, diff)
+
+    if carbon_current != carbon_previous: 
+        diff = carbon_previous - carbon_current
+        db.insertCarbonPrice(carbon_current, diff)
+    return 
+
+def GetAndRefineNewPrices():
     t = getter.getPrices()
     arr = np.array(t)
 
-    cp_raw = arr[0].split()
-    ss_raw = arr[1].split()
+    ss_raw = arr[0].split()
+    cp_raw = arr[1].split()
 
     cp = float(re.findall( r'\d+\.*\d*', cp_raw[0])[0])
     ss = float(re.findall( r'\d+\.*\d*', ss_raw[0])[0])
     return np.array([cp,ss])
+
+findPrices()
