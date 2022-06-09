5c5e7fd8877f906160362e349b4606a530a7e63d
Edi Rose <edirose1998@gmail.com>
2022-06-09 21:56:15 +1200

    added headless - added time contraints to listen

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import priceGetters as getter
 import numpy as np 
 import re
 import db_connect as db
-import datetime
+from datetime import datetime
 import time
 
 
 def listen():
-    while True: 
-        print('checking for new prices')
-        try:
-            kickOff()
-        except:
-            print('find price failed')
-        else:
-            print('check complete')
-        time.sleep(300)
+    day =  datetime.now().weekday() # 0 is Monday 6, is Sunday
+    hour = datetime.now().time().hour
+    while 18 > hour > 7 & day < 5:  
+        marketIsOpen = getter.getCarbonIsOpen()
+        while marketIsOpen: 
+            print('checking for new prices')
+            try:
+                kickOff()
+            except:
+                print('find price failed')
+            else:
+                print('check complete')
+                time.sleep(300)
+    else:
+        print('market is closed')
+        time.sleep(3600)
 
 def GetAndRefineNewPrices():
     t = getter.getPrices()
@@ -31,7 +38,7 @@ def GetAndRefineNewPrices():
 
 
 def kickOff(): 
-    dt = datetime.datetime
+    dt = datetime
     carbon_previous = db.getLatestCarbonFromDB()[2]
     salt_previous = db.getLatestSaltFromDB()[2]
 
@@ -42,13 +49,20 @@ def kickOff():
     if salt_current != salt_previous: 
         print('new salt price detected at: ', dt)
         diff = salt_current - salt_previous
-        db.insertSaltPrice(salt_current, diff)
+        try: 
+            db.insertSaltPrice(salt_current, diff)
+        except: 
+            print('adding salt price failed')
 
     if carbon_current != carbon_previous: 
         print('new carbon price detected at: ', dt)
-        db.insertCarbonPrice(carbon_current, carbon_previous, salt_current)
+        try:
+            db.insertCarbonPrice(carbon_current, carbon_previous, salt_current)
+        except: 
+            print('adding carbon price failed')
     else:
         print('no new price detected, waiting 5 minutes')
     return True 
 
+#this starts erryythang
 listen()
--- priceGetters.py
+++ priceGetters.py
@@ -1,12 +1,30 @@
-
-from lib2to3.pgen2.driver import Driver
-from matplotlib.pyplot import get
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+chrome_driver = '/Users/edirose/Desktop/drivers/chromedriver'
+options = Options()
+options.headless = True
+
+def getCarbonIsOpen():
+    driver = getHeadlessDriver()
+    driver.get("https://commtrade.co.nz")
+    try:  
+        isOpen = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.ID, "marketstatus"))
+        ).text
+    except:
+        print('could not find out whether market is open or not')
+    finally:
+        if isOpen == "MARKET IS OPEN":
+            return True
+        if isOpen == "MARKET IS CLOSED":
+            return False
+        else:
+            print('GetCarbonIsOpen returned with unexpected status: ', isOpen)
+            return False
 
 def getCarbonPrice(driver):
     driver.get("https://commtrade.co.nz")
@@ -14,7 +32,10 @@ def getCarbonPrice(driver):
         cp_raw = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.ID, "ctl00_Products_Td3_0"))
         ).text
+    except: 
+        print('could not getCarbonPrice')
     finally:
+        print(cp_raw)
         return cp_raw
 
 def getSaltSharePrice(driver):   
@@ -23,18 +44,28 @@ def getSaltSharePrice(driver):
         ss = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "/html/body/section/div[2]/div/section[1]/div/div[1]/h1"))
         ).text
+    except: 
+        print('could not getSaltSharePrice')
     finally:
+        print(ss)
         return ss
 
 def getPrices():
-    driver = getDriver()
+    driver = getHeadlessDriver()
     try: 
         cp_raw= getCarbonPrice(driver)
+    except:
+        print('get Carbon failed')
+    try: 
+         ss= getSaltSharePrice(driver)
+    except: 
+        print('getSaltShare failed')
     finally: 
-        ss = getSaltSharePrice(driver)
-        v=  [ss, cp_raw]
+        v=[ss, cp_raw]
         driver.close()
+        print(v)
     return v
 
-def getDriver():
-    return webdriver.Safari()
+
+def getHeadlessDriver():
+    return webdriver.Chrome(chrome_driver, options=options)
