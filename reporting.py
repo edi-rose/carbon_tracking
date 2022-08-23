0e88cc0218f419c13e2b2c1a45059b04cbe41794
Edi Rose <edirose1998@gmail.com>
2022-08-23 20:36:51 +1200

    can now plot the carbon price

--- reporting.py
+++ reporting.py
@@ -1,2 +1,19 @@
-import db_connect as db
 
+import db_connect as db
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+from datetime import datetime as dt
+
+
+def plotAllCarbon(): 
+    data = json.loads(db.getAllCarbonFromDB()[0][0])
+    dates = []
+    prices = []
+    for x in data:
+        dates.append(dt.strptime(x["date"],'%Y-%m-%d %H:%M:%S.%f'))
+        prices.append(x["value"])
+
+    plt.plot(dates, prices)
+    plt.show()
+    return data
