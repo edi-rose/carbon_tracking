d126d473e67f0f565ccb88382c4ff5975e13afb8
Edi Rose <edirose1998@gmail.com>
2022-12-13 15:26:57 +1300

    added new date helpers

--- data_helpers.py
+++ data_helpers.py
@@ -1,7 +1,19 @@
 import re
 from datetime import datetime as dt
+from datetime import timedelta, date
 import json
 
+def daterange(date1, date2):
+    for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+
+def getDatesBetween(start, end):
+    dates = []
+    start_dt = dt.strptime(start,'%Y-%m-%d')
+    end_dt = dt.strptime(end,'%Y-%m-%d')
+    for date in daterange(start_dt, end_dt):
+        dates.append(date.strftime("%Y-%m-%d"))
+    return dates
 # turn price from $1.2 -> 1.2
 def cleanPrice(price):
     raw = price.split()
@@ -21,5 +33,12 @@ def sortData(data: dict):
     sorted_dict = dict(sorted(data.items()))
     return sorted_dict
 
-def refinePricesForReports(data):
+def refinePricesForReports(data:dict):
     return sortData(convertToDatetime(json.loads(data)))
+
+def changeDateFromSlashesToDashes(date):
+    str = dt.strptime(date, '%d/%m/%Y').strftime('%d-%m-%Y')
+    return dt.strptime(str, '%d-%m-%Y')
+
+def dateStringToDatetime(date):
+    return dt.strptime(date, '%Y-%m-%d')
